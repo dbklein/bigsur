daa74f444a48dc296e6edd27f0be6207a4e057b1
David Klein <dk@generalassemb.ly>
2015-05-15 18:23:23 -0400

    added posts model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512190114) do
+ActiveRecord::Schema.define(version: 20150515221637) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.string   "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
