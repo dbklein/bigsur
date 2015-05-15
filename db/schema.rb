44e523e40db78174d9e99851d1aeb4e5bf72ba07
David Klein <dk@generalassemb.ly>
2015-05-15 19:01:41 -0400

    removed author from posts bc author should just be a user so I can add an association later

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150515221637) do
+ActiveRecord::Schema.define(version: 20150515230036) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
-    t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
