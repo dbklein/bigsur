6df74b08d0a873f9b95c24f249b20fbc1fc66418
David Klein <dk@generalassemb.ly>
2015-05-15 18:50:54 -0400

    added posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/1
+  # GET /posts/1.json
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+end
